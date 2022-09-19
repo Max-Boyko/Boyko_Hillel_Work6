2b0bb5055fdd989fa543b051f602da94ba6aea16
Max Boyko <makcboukomail@gmail.com>
2022-09-20 00:57:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Li");
+
+        int warrior = 13;
+        int archer = 24;
+        int rider = 46;
+        int a = 860;
+
+        int Li = (warrior + archer + rider) * a;
+        System.out.println(Li);
+
+        System.out.println();
+
+        System.out.println("Min");
+
+        int warrior2 = 9;
+        int archer2 = 35;
+        int rider2 = 12;
+        final double b = 1.5;
+
+        int Min = (warrior2 + archer2 + rider2);
+                double Min2 = Min *b;
+
+        System.out.println((int)Min2);
+    }
+}
